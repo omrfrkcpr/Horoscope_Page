6b98bb704ccb3471da79855135015c2010ee954f
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-23 13:36:04 +0100

    chore: add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.scss";
 import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import Card from "./components/main/Card";
+import Footer from "./components/footer/Footer";
+import Slider from "./components/slider/Slider";
 
 const App = () => {
   //? it should return only one div. But inside it it can be lots of divs !
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Slider />
+      <Header />
+      <Card />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from "../../assets/general/logo.png";
+import "../navbar/Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="nav d-flex justify-content-between align-items-center  p-4">
+      <div className="nav--left">
+        <img src={logo} alt="" width="150px" className="logo" />
+      </div>
+      <div className="nav--middle">
+        <h1>Horoscope</h1>
+      </div>
+      <div className="nav--right">
+        <ul className="pages list-unstyled d-flex fs-4 mt-3">
+          <li className="me-3">
+            <a href="#body">Home</a>
+          </li>
+          <li className="me-3">
+            <a href="#tarot">Tarot</a>
+          </li>
+          <li className="me-3">
+            <a href="#astrology">Astrology</a>
+          </li>
+          <li className="me-3">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
