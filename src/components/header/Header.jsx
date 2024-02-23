b3f5c79ec8340d43c7bec76ded70cd597d802286
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-23 14:55:56 +0100

    chore: add header.jsx and its scss

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import "../header/Header.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="title text-center">
+      <h1>What's your horoscope?</h1>
+    </div>
+  );
 };
 
 export default Header;
