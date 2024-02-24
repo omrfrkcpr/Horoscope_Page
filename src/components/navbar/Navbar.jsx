2a0a8cd6eb5860952e5df011fcded63a44816d18
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-24 21:06:26 +0100

    navbar - add toggle button functionality

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import logo from "../../assets/general/logo.png";
 import "../navbar/Navbar.scss";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef(null);
+  const buttonRef = useRef(null);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+
+  const handleOutsideClick = (event) => {
+    // Menü açıkken ve tıklanan öğe menü dışında ve buton dışında ise menüyü kapat
+    if (
+      showMenu &&
+      !menuRef.current.contains(event.target) &&
+      !buttonRef.current.contains(event.target)
+    ) {
+      setShowMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    // Body'ye tıklama olayını dinle
+    document.body.addEventListener("click", handleOutsideClick);
+
+    // Cleanup: Component kaldırıldığında event listener'ı temizle
+    return () => {
+      document.body.removeEventListener("click", handleOutsideClick);
+    };
+  }, [showMenu]);
 
   return (
     <div className="nav">
@@ -17,8 +40,8 @@ const Navbar = () => {
       <div className="nav--middle">
         <h1>Horoscope</h1>
       </div>
-      <div className="nav--right">
-        <ul className={`pages list-unstyled  ${showMenu ? "show" : ""}`}>
+      <div className="nav--right" ref={menuRef}>
+        <ul className={`pages list-unstyled ${showMenu ? "show" : ""}`}>
           <li className="me-1">
             <a href="#home" className="w-100">
               Home
@@ -35,9 +58,9 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-      <div className="nav--toggle">
+      <div className="nav--toggle" ref={buttonRef}>
         <button onClick={toggleMenu} className="px-3 py-2 border-0 fs-4 me-3">
-          <i class="fa fa-bars" aria-hidden="true"></i>
+          <i className="fa fa-bars" aria-hidden="true"></i>
         </button>
       </div>
     </div>
